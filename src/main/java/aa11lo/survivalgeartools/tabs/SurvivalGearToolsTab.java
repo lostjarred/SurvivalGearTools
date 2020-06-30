c26ef828d806aeae24454602a2da46a4ff5b739a
lostjarred <lostotaku@outlook.com>
2020-07-01 08:52:28 +1000

    Added model and texture for survivalaxe

--- src/main/java/aa11lo/survivalgeartools/tabs/SurvivalGearToolsTab.java
+++ src/main/java/aa11lo/survivalgeartools/tabs/SurvivalGearToolsTab.java
@@ -17,6 +17,6 @@ public class SurvivalGearToolsTab extends CreativeTabs {
     @SideOnly(Side.CLIENT)
     @Override
     public ItemStack createIcon(){
-        return new ItemStack(ModItems.SURIVIVAL_AXE);
+        return new ItemStack(ModItems.TOOL_SURIVIVAL_AXE);
     }
 }
