5a47eafe3ddedca28263b2e384c90883fb9dac9e
lostjarred <lostotaku@outlook.com>
2020-07-03 13:32:58 +1000

    Added Survival Knife

---
+++ src/main/java/aa11lo/survivalgeartools/item/ItemSurvivalToolKnife.java
@@ -0,0 +1,11 @@
+package aa11lo.survivalgeartools.item;
+
+import aa11lo.survivalgeartools.material.SurvivalGearToolsMaterials;
+import net.minecraft.item.ItemSword;
+
+public class ItemSurvivalToolKnife extends ItemSword {
+    ItemSurvivalToolKnife(){
+        super(SurvivalGearToolsMaterials.SURVIVAL_TOOL);
+    }
+
+}
