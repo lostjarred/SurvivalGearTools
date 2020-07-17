3a31b3ed6935bf3b9c6eb3d834edeae89f09e652
lostjarred <lostotaku@outlook.com>
2020-07-18 08:20:16 +1000

    Survival Shovel can now make farm land

--- src/main/java/aa11lo/survivalgeartools/item/ItemSurvivalToolsShovel.java
+++ src/main/java/aa11lo/survivalgeartools/item/ItemSurvivalToolsShovel.java
@@ -1,7 +1,21 @@
 package aa11lo.survivalgeartools.item;
 
 import aa11lo.survivalgeartools.survivalgeartools;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemSpade;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ItemSurvivalToolsShovel extends ItemSpade {
     
@@ -11,6 +25,46 @@ public class ItemSurvivalToolsShovel extends ItemSpade {
         setTranslationKey(survivalgeartools.MODID + "." + registryName);
         setRegistryName(registryName);
         setCreativeTab(survivalgeartools.SurvivalGearToolsCreativeTab);
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        ItemStack itemstack = player.getHeldItem(hand);
+
+        if (!player.canPlayerEdit(pos.offset(facing), facing, itemstack))
+        {
+            return EnumActionResult.FAIL;
+        }
+        else
+        {
+            IBlockState iblockstate = worldIn.getBlockState(pos);
+            Block block = iblockstate.getBlock();
+
+            if (facing != EnumFacing.DOWN && worldIn.getBlockState(pos.up()).getMaterial() == Material.AIR && block == Blocks.GRASS)
+            {
+                IBlockState iblockstate1 = Blocks.GRASS_PATH.getDefaultState();
+                worldIn.playSound(player, pos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
+                if (!worldIn.isRemote)
+                {
+                    worldIn.setBlockState(pos, iblockstate1, 11);
+                    itemstack.damageItem(1, player);
+                }
+
+                return EnumActionResult.SUCCESS;
+            }
+            if (facing != EnumFacing.DOWN && worldIn.getBlockState(pos.up()).getMaterial() == Material.AIR && block == Blocks.GRASS_PATH || block == Blocks.DIRT) {
+                if (!worldIn.isRemote){
+                    worldIn.setBlockState(pos, Blocks.FARMLAND.getDefaultState(), 11);
+                }
+                return EnumActionResult.SUCCESS;
+            }
+            else
+            {
+                return EnumActionResult.PASS;
+            }
+        }
     }
 
 }
